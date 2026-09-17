d6ed48db8eecddbbd033715c1024aa03f0d84ea2
Shubham Dey <xlegendytx999@gmail.com>
2026-09-17 16:07:59 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './shubhamcooks_',
+  base: '/shubhamcooks/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
